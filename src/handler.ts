4c541aa879e279f8d3a683086b8948172543994d
justcabyr <asulaiman@dervac.com>
2022-03-22 01:54:07 +0100

    feat: Added event to createItem

--- src/handler.ts
+++ src/handler.ts
@@ -1,17 +1,47 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import AWS from 'aws-sdk';
+import { handleError } from './middleware/HttpError';
+import { v4 } from 'uuid';
+import * as yup from 'yup';
 
-export const hello = async (
+const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = 'ItemsTable';
+
+export const headers = {
+  'content-type': 'application/json',
+};
+
+const schema = yup.object().shape({
+  label: yup.string().required(),
+  completed: yup.bool().required(),
+});
+
+export const createItem = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  try {
+    const reqBody = JSON.parse(event.body as string);
+
+    await schema.validate(reqBody, { abortEarly: false });
+
+    const item = {
+      ...reqBody,
+      itemID: v4(),
+    };
+
+    await docClient
+      .put({
+        TableName: tableName,
+        Item: item,
+      })
+      .promise();
+
+    return {
+      statusCode: 201,
+      headers,
+      body: JSON.stringify(item),
+    };
+  } catch (e) {
+    return handleError(e);
+  }
 };
